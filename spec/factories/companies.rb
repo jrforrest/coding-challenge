2ed9e72d95cbbfb51068ac4f22c93e55de76a740
Jack Forrest <jack@jrforrest.net>
2017-03-14 19:44:20 -0400

    Adds company founders to seeds

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -5,5 +5,11 @@ FactoryGirl.define do
     state { Faker::Address.state }
     description { Faker::Lorem.paragraphs.join("\n") }
     founded_at { rand(1..8).weeks.ago }
+
+    trait :with_founders do
+      after(:build) do |company|
+        rand(1..3).times { company.founders << build(:founder, company: company) }
+      end
+    end
   end
 end
