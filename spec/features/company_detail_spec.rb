114f11b45c40c90a13302560d137198a0fa611c5
Jack Forrest <jack@jrforrest.net>
2017-03-14 13:46:54 -0400

    Adds company detail page
    - Adds Show route, view and action for Company
    - Makes company title in index link to details page
    - Styles details page
    - Specs detail page and navigation from index
    - Adds some basic helpers for date and company location to
      ApplicationHelpers
    
    Styles in main.sass are getting a little hefty at this point, further
    development should perhaps attempt to abstract some flexbox logic, or at
    least break page-specific styles into their own sass files.

---
+++ spec/features/company_detail_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'Viewing company details' do
+  let!(:company) { create(:company) }
+
+  describe 'clicking a company name from the home page' do
+    before do
+      visit '/'
+      click_link company.name
+    end
+
+    it 'brings me to company detail by clicking a company name' do
+      expect(current_path).to eql(company_path(company))
+    end
+  end
+
+  describe 'the company detail page' do
+    include ApplicationHelper
+
+    let(:location_string) { company_location(company) }
+    let(:date_string) { date(company.founded_at) }
+    let(:description_string) { company.description.gsub("\n", ' ') }
+
+    before { visit company_path(company) }
+    subject { page }
+
+    it { is_expected.to have_selector('.location', text: location_string) }
+    it { is_expected.to have_selector('.name', text: company.name) }
+    it { is_expected.to have_selector('.founded_at', text: date_string) }
+    it { is_expected.to have_selector('.description', text: description_string) }
+  end
+end
