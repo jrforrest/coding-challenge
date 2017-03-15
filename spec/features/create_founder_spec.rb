027b667761da8f6271d093510e89eb55280ac1f2
Jack Forrest <jack@jrforrest.net>
2017-03-14 20:33:39 -0400

    Allows a founder to be added to a company
    - Adds founder create route and controller action
    - Creates new founder view
    - Creates partial for new founder form
    - Adds new founder form to company details page
    - Specs founder creation

---
+++ spec/features/create_founder_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe 'Adding a founder to a company' do
+  let(:company) { create(:company) }
+
+  before do
+    visit company_path(company)
+    fill_in 'Full name', with: input_name
+    fill_in 'Position', with: input_position
+    click_button 'save'
+  end
+
+  shared_examples_for :save_success do
+    it 'sends me to the details page' do
+      expect(current_path).to eql(company_path(company))
+    end
+
+    it 'shows a success message' do
+      expect(page).to have_selector('.flash-alert.success')
+    end
+
+    it 'shows the new founder' do
+      expect(page).
+        to have_selector('.founder', text: 'Jack Forrest: Bashtronaut')
+    end
+  end
+
+  context 'when the form is submitted with valid values' do
+    let(:input_name) { 'Jack Forrest' }
+    let(:input_position) { 'Bashtronaut' }
+
+    it_behaves_like :save_success
+  end
+
+  context 'When the form is submitted with invalid values' do
+    let(:input_name) { 'Jack Forrest' }
+    let(:input_position) { '' }
+
+    it 'sends me to a new page to correct the mistakes' do
+      expect(page).to have_selector('h2', text: 'Derp')
+    end
+
+    context 'When I correct the issues and continue' do
+      before do
+        fill_in 'Position', with: 'Bashtronaut'
+        click_button 'save'
+      end
+
+      it_behaves_like :save_success
+    end
+  end
+end
